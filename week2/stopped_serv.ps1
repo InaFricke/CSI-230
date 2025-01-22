ffe0a1238db30a0ee2a360b132d44a8fed23733d
InaFricke <ina.fricke@mymail.champlain.edu>
2025-01-22 14:04:50 -0500

    week2

---
+++ week2/stopped_serv.ps1
@@ -0,0 +1,7 @@
+﻿$stoppedServices = Get-Service | Where-Object {$_.Status -eq "Stopped"} | Sort-Object -Property Name
+
+# get all stopped services name them stopped services, sort services alphabetically  by name
+
+#Export to CSV
+
+$stoppedServices | Export-csv -Path "C:\Users\champuser\SYS320-01\week2\stopped_serv.csv" 
