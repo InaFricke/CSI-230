8138c63cf9b55c761664936cf1a1dc675af66819
InaFricke <ina.fricke@mymail.champlain.edu>
2025-01-22 13:52:22 -0500

    Champ-Chrome Script

---
+++ week2/champlain.ps1
@@ -0,0 +1,7 @@
+﻿#check if chrome is running assign it
+$chromePro = Get-Process -Name "chrome"
+if ($chromePro -eq $null) {
+Start-Process "chrome.exe" "https://www.champlain.edu/"
+} else {
+Stop-Process -Name "chrome" -Force
+}
